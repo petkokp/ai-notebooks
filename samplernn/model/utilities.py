1e4bbcf980b88f9e355469c493fdd04ab08953b8
petkokp <petko.petkov987@gmail.com>
2023-09-07 12:17:25 +0300

    added predictor helper class

---
+++ samplernn/model/utilities.py
@@ -0,0 +1,16 @@
+def linear_quantize(samples, q_levels):
+    EPSILON = 1e-2
+    samples = samples.clone()
+    samples -= samples.min(dim=-1)[0].expand_as(samples)
+    samples /= samples.max(dim=-1)[0].expand_as(samples)
+    samples *= q_levels - EPSILON
+    samples += EPSILON / 2
+    return samples.long()
+
+
+def linear_dequantize(samples, q_levels):
+    return samples.float() / (q_levels / 2) - 1
+
+
+def q_zero(q_levels):
+    return q_levels // 2
