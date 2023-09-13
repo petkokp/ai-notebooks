41f1799ef759405d3f24ef2624c41bc90c9538a0
petkokp <petko.petkov987@gmail.com>
2023-09-13 18:59:45 +0300

    implemented memory for the network

---
+++ neural-turing-machine/model/memory.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+import numpy as np
+
+
+def _convolve(w, s):
+    assert s.size(0) == 3
+    t = torch.cat([w[-1:], w, w[:1]])
+    return F.conv1d(t.view(1, 1, -1), s.view(1, 1, -1)).view(-1)
+
+
+class NTMMemory(nn.Module):
+    def __init__(self, N, M):
+        super(NTMMemory, self).__init__()
+
+        self.N = N
+        self.M = M
+
+        self.register_buffer('mem_bias', torch.Tensor(N, M))
+
+        stdev = 1 / (np.sqrt(N + M))
+        nn.init.uniform_(self.mem_bias, -stdev, stdev)
+
+    def reset(self, batch_size):
+        self.batch_size = batch_size
+        self.memory = self.mem_bias.clone().repeat(batch_size, 1, 1)
+
+    def size(self):
+        return self.N, self.M
+
+    def read(self, w):
+        return torch.matmul(w.unsqueeze(1), self.memory).squeeze(1)
+
+    def write(self, w, e, a):
+        self.prev_mem = self.memory
+        self.memory = torch.Tensor(self.batch_size, self.N, self.M)
+        erase = torch.matmul(w.unsqueeze(-1), e.unsqueeze(1))
+        add = torch.matmul(w.unsqueeze(-1), a.unsqueeze(1))
+        self.memory = self.prev_mem * (1 - erase) + add
+
+    def address(self, k, β, g, s, γ, w_prev):
+        wc = self._similarity(k, β)
+        wg = self._interpolate(w_prev, wc, g)
+        ŵ = self._shift(wg, s)
+        w = self._sharpen(ŵ, γ)
+
+        return w
+
+    def _similarity(self, k, β):
+        k = k.view(self.batch_size, 1, -1)
+        w = F.softmax(β * F.cosine_similarity(self.memory +
+                      1e-16, k + 1e-16, dim=-1), dim=1)
+        return w
+
+    def _interpolate(self, w_prev, wc, g):
+        return g * wc + (1 - g) * w_prev
+
+    def _shift(self, wg, s):
+        result = torch.zeros(wg.size())
+        for b in range(self.batch_size):
+            result[b] = _convolve(wg[b], s[b])
+        return result
+
+    def _sharpen(self, ŵ, γ):
+        w = ŵ ** γ
+        w = torch.div(w, torch.sum(w, dim=1).view(-1, 1) + 1e-16)
+        return w
